855f99bc08987f72d442997910d69225a4b58575
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-04-30 16:47:01 +0300

    add trackBy for rendering

--- src/app/channels/components/channels-body/channels-body.component.ts
+++ src/app/channels/components/channels-body/channels-body.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
-import { Channel } from 'src/app/shared';
+import { Channel, TrackByUtil } from 'src/app/shared';
 
 @Component({
   selector: 'app-channels-body',
@@ -8,7 +8,7 @@ import { Channel } from 'src/app/shared';
   styleUrls: ['./channels-body.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChannelsBodyComponent {
+export class ChannelsBodyComponent extends TrackByUtil<Channel> {
   @Input() starred: boolean;
   @Input() channels: Channel[];
   @Input() prefix: string = '#';
