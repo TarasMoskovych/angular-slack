c2e1ad9caaab13f9d9bae909ce3fc5e1c3836e2b
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-05-04 20:56:01 +0300

    adjust messages-header component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Channel } from 'src/app/shared';
+import {
+  channelsSelectedSelector,
+  selectedStarredSelector,
+  StarChannel,
+  starredChannelsLengthSelector,
+} from 'src/app/+store';
 
 @Component({
   selector: 'app-messages',
@@ -7,10 +17,21 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesComponent implements OnInit {
+  channel$: Observable<Channel>;
+  isStarred$: Observable<boolean>;
+  starredChannelsLength$: Observable<number>;
 
-  constructor() { }
+  constructor(
+    private store: Store,
+  ) { }
 
   ngOnInit(): void {
+    this.channel$ = this.store.select(channelsSelectedSelector);
+    this.isStarred$ = this.store.select(selectedStarredSelector);
+    this.starredChannelsLength$ = this.store.select(starredChannelsLengthSelector);
   }
 
+  onStar({ channel, starred }: { channel: Channel, starred: boolean }): void {
+    this.store.dispatch(new StarChannel({ [channel.id]: starred }));
+  }
 }
