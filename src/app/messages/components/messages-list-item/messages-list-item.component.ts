37cee2b4da45e295df69932f7a095083702b9bfc
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-05-20 19:38:51 +0300

    add scroll into view

--- src/app/messages/components/messages-list-item/messages-list-item.component.ts
+++ src/app/messages/components/messages-list-item/messages-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ElementRef } from '@angular/core';
 
 import { Message, User } from 'src/app/shared';
 
@@ -12,9 +12,14 @@ export class MessagesListItemComponent implements OnInit {
   @Input() message: Message;
   @Input() user: User;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+
+  }
+
+  scrollIntoView(): void {
+    this.elementRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
   }
 
   get outcome(): boolean {
--- src/app/messages/components/messages-list/messages-list.component.ts
+++ src/app/messages/components/messages-list/messages-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ViewChildren, QueryList } from '@angular/core';
 
 import { Message, TrackByUtil, User } from 'src/app/shared';
+import { MessagesListItemComponent } from '../messages-list-item/messages-list-item.component';
 
 @Component({
   selector: 'app-messages-list',
@@ -11,8 +12,14 @@ import { Message, TrackByUtil, User } from 'src/app/shared';
 export class MessagesListComponent extends TrackByUtil<Message> implements OnInit {
   @Input() messages: Message[] = [];
   @Input() user: User;
+  @ViewChildren(MessagesListItemComponent) list: QueryList<MessagesListItemComponent>;
 
   ngOnInit(): void {
   }
 
+  ngOnChanges() {
+    if (this.list?.last) {
+      setTimeout(() => this.list.last.scrollIntoView(), 0);
+    }
+  }
 }
